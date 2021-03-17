d17fa94c1ce06042ddb0f0098203921e93d4ab63
tombill143 <tombill143@gmail.com>
2021-03-17 16:54:00 +0100

    Group class

---
+++ src/main/java/Model/Group.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class Group {
+    private String groupName;
+
+    public Group(String groupName){
+        this.groupName = groupName;
+    }
+
+    public String toString(){
+        return groupName;
+    }
+
+}
+
