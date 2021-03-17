a169f518e0d9046fdc88b81049e4b15447140635
justaszdanavicius <zdanaviciusjustas1@gmail.com>
2021-03-17 13:12:12 +0100

    Created Address class

---
+++ src/Model/Address.java
@@ -0,0 +1,38 @@
+package Model;
+
+public class Address {
+    private String city;
+    private int postCode;
+    private String streetName;
+    private String streetNumber;
+    private int floorNumber;
+    private String apartmentNumber;
+    private String careOfName;
+    private String careOfSurname;
+
+    public Address(String city,
+                   int postCode,
+                   String streetName,
+                   String streetNumber,
+                   int floorNumber,
+                   String apartmentNumber,
+                   String careOfName,
+                   String careOfSurname) {
+        this.city = city;
+        this.postCode = postCode;
+        this.streetName = streetName;
+        this.streetNumber = streetNumber;
+        this.floorNumber = floorNumber;
+        this.apartmentNumber = apartmentNumber;
+        this.careOfName = careOfName;
+        this.careOfSurname = careOfSurname;
+    }
+
+    public String toString(){
+        return streetName + " " +
+                streetNumber + " " +
+                floorNumber + "," +
+                apartmentNumber + ", " +
+                postCode;
+    }
+}
