03bdce3e695162820f6e11f7dfce1d7cec7d2d39
Sintry1 <Dominic_smith2012@hotmail.co.uk>
2021-03-22 16:13:29 +0100

    Added the remainder of the remove methods based on the templates already in the RemoveController

--- src/main/java/build/Controller/RemoveController.java
+++ src/main/java/build/Controller/RemoveController.java
@@ -36,4 +36,52 @@ public class RemoveController extends Controller {
             Daycare.model.getChildList().remove(child);
         }
     }
+
+    public static void removeGuardian(){
+        System.out.println("- Removing a parent -");
+
+        Parent parent = Daycare.model.getParentList().select(Daycare.scanner);
+
+        if(parent == null){
+            System.out.println("Insufficient information, exiting.");
+            return;
+        }
+
+        String confirmation = inputString("Are you sure you want to remove the following parent from the system (Yes/No)?\n" + parent + "\n", new String[]{"Yes", "No"});
+        if(confirmation.equals("Yes")){
+            Daycare.model.getParentList().remove(parent);
+        }
+    }
+
+    public static void removeClass(){
+        System.out.println("- Removing a Class -");
+
+        Group group = Daycare.model.getGroupList().select(Daycare.scanner);
+
+        if(group == null){
+            System.out.println("Insufficient information, exiting.");
+            return;
+        }
+
+        String confirmation = inputString("Are you sure you want to remove the following group from the system (Yes/No)?\n" + group + "\n", new String[]{"Yes", "No"});
+        if(confirmation.equals("Yes")){
+            Daycare.model.getGroupList().remove(group);
+        }
+    }
+
+    public static void removeTeacher(){
+        System.out.println("- Removing a Teacher -");
+
+        Teacher teacher = Daycare.model.getTeacherList().select(Daycare.scanner);
+
+        if(teacher == null){
+            System.out.println("Insufficient information, exiting.");
+            return;
+        }
+
+        String confirmation = inputString("Are you sure you want to remove the following teacher from the system (Yes/No)?\n" + teacher + "\n", new String[]{"Yes", "No"});
+        if(confirmation.equals("Yes")){
+            Daycare.model.getTeacherList().remove(teacher);
+        }
+    }
 }
